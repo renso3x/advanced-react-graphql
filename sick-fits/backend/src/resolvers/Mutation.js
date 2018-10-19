0b132afb24c42d2541f12c34a5f9b39756ca0f08
Romeo Enso <romeo.enso@viseo.com>
2018-10-19 22:00:31 +0800

    graphql delete mutation, query

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -9,6 +9,27 @@ const Mutations = {
 
     return item
   },
+  async updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    return await ctx.db.mutation.updateItem({
+      data: {
+        ...updates
+      },
+      where: {
+        id: args.id
+      }
+    }, info)
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    //find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // check if they own that item, or have the permissions
+    // TODO
+    // delete item
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
 };
 
 module.exports = Mutations;
