9d9ca698c5fc3cdd059b346d3bce846226c58730
Romeo Enso <romeo.enso@viseo.com>
2018-11-09 09:32:30 +0800

    cart component, apollo local state query mutation, toggle cart

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { Mutation } from 'react-apollo';
+import { TOGGLE_CART_MUTATION } from './Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
 import Signout from './Signout';
@@ -19,6 +21,11 @@ const Nav = () => (
               <a>Orders</a>
             </Link>
             <Signout />
+            <Mutation mutation={TOGGLE_CART_MUTATION}>
+              {(toggleCart) => (
+                <button onClick={toggleCart}>My Cart</button>
+              )}
+            </Mutation>
           </>
         )}
         {!me && (
