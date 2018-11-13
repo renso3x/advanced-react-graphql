ccf7dbb4473d856447db1ff6b3ef842b4c4f322a
Romeo Enso <romeo.enso@viseo.com>
2018-11-13 10:20:05 +0800

    optimistic and update cache on remove item

--- sick-fits/frontend/components/RemoveCartitem.js
+++ sick-fits/frontend/components/RemoveCartitem.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import { CURRENT_USER_QUERY } from './User';
 
 const REMOVE_ITEM_MUTATION = gql`
-  mutation REMOVE_ITEM_MUTATION ($id: ID!) {
+    mutation REMOVE_ITEM_MUTATION ($id: ID!) {
     removeToCart(id: $id) {
       id
     } 
@@ -27,11 +28,30 @@ class RemoveCartitem extends Component {
     id: PropTypes.string.isRequired
   };
 
+  update = (cache, payload) => {
+    //fetch the user query
+    const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+    //get the payload id
+    const cartItemId = payload.data.removeToCart.id;
+    // remove the cart
+    data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+    // update the cache
+    cache.writeData({ query: CURRENT_USER_QUERY, data });
+  }
+
   render() {
     return (
       <Mutation
         mutation={REMOVE_ITEM_MUTATION}
         variables={{ id: this.props.id }}
+        update={this.update}
+        optimisticResponse={{
+          __typename: 'Mutation',
+          removeToCart: {
+            __typename: 'CartItem', // type schema
+            id: this.props.id
+          }
+        }}
       >
         {(removeToCart, { error, loading }) => (
           <BigButton
