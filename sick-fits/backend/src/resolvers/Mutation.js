d6f53ecd6a97f7e77b5ac3e2cd952eef2cebe5fa
Romeo Enso <romeo.enso@viseo.com>
2018-11-13 09:56:04 +0800

    remove cart mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -256,6 +256,17 @@ const Mutations = {
         }
       }
     }, info);
+  },
+  async removeToCart(parent, args, ctx, info) {
+    const cartItem = await ctx.db.query.cartItem({
+      where: { id: args.id }
+    }, `{ id, user { id } }`);
+
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error('You are not allowed to delete an item.');
+    }
+
+    return ctx.db.mutation.deleteCartItem({ where: { id: cartItem.id } });
   }
 };
 
